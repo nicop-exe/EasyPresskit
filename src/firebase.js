fa5257482ef2f4bee75a7e6da26ed6f36b661b19
nicop-exe <109947815+nicop-exe@users.noreply.github.com>
2026-02-14 15:20:57 +0100

    fix: restore firebaseConfig to fix blank screen

--- src/firebase.js
+++ src/firebase.js
@@ -1,4 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+const firebaseConfig = {
+    apiKey: "AIzaSyCdpqREVda-R82LHaaRLd-sicgnLy7vuO0",
+    authDomain: "easypresskit.firebaseapp.com",
+    projectId: "easypresskit",
+    storageBucket: "easypresskit.firebasestorage.app",
+    messagingSenderId: "491409397664",
+    appId: "1:491409397664:web:ec2b6197aaf78322edf6c5",
+    measurementId: "G-Y3JP68TEYH"
+};
+
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
